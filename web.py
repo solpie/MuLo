4927a5ae63f785936c231485e9ff2664a52f88b9
solpie <solpie.net@gmail.com>
2015-04-20 09:08:14 +0800

    run

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 from bottle import get, run, static_file, post, route, request, redirect
 
-
+import sys
 @get('/')
 def index():
     return 'hello mulo'
+
+
+if len(sys.argv) > 1:
+    run(host='0.0.0.0', port=sys.argv[1], debug=True)
+else:
+    run(host='127.0.0.1', reloader=True, debug=True)
